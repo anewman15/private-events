e081001ced4df18e895342e2f87aa6d68bf5d2e3
Abdullah Numan <anewman15@hotmail.com>
2020-10-02 12:52:47 +0600

    Create event views

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'users/registrations', sessions: 'users/sessions' }
   resources :users, only: [:new, :create, :show]
+  resources :events
   
   devise_scope :user do
     root 'users/registrations#new'
