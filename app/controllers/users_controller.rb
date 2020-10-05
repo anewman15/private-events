a1f77ed4ec32f4ca71e9b1030e10fe57d7820df3
Abdullah Numan <anewman15@hotmail.com>
2020-10-05 16:52:00 +0600

    Update index and user page with upcoming and past events

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,9 @@
 class UsersController < ApplicationController
   
   def show
-    @user_events = current_user.created_events
-    @user_attended_events = current_user.attended_events
+    @user_created_events_upcoming = current_user.created_events.upcoming_events
+    @user_created_events_past = current_user.created_events.past_events
+    @user_attended_events_upcoming = current_user.created_events.upcoming_events
+    @user_attended_events_past = current_user.attended_events.past_events
   end
 end
