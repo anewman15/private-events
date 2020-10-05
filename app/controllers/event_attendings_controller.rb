bd1b0e3e40aa7fe72eb3b8ce4100045f07eed69a
Abdullah Numan <anewman15@hotmail.com>
2020-10-05 19:58:24 +0600

    Create helper methods for Attend Event buttons, add flash messages

--- app/controllers/event_attendings_controller.rb
+++ app/controllers/event_attendings_controller.rb
@@ -3,14 +3,21 @@ class EventAttendingsController < ApplicationController
 
   def create
     # byebug
-    @event_attending = EventAttending.new(event_attendee_id: current_user.id, attended_event_id: params[:attended_event_id])
+    attended_event = Event.find(params[:attended_event_id])
+    if (attended_event.event_date - Time.now)/3600.round > 0
+      @event_attending = EventAttending.new(event_attendee_id: current_user.id, attended_event_id: params[:attended_event_id])
+    else
+      flash[:alert] = "You cannot attend a past event"
+      redirect_to event_path(params[:attended_event_id])
+      return
+    end
 
     respond_to do |format|
       if @event_attending.save
-        format.html { redirect_to event_path(params[:attended_event_id]), notice: 'You decided to attend this event.' }
+        format.html { redirect_to event_path(params[:attended_event_id]), notice: 'You decided to attend this event' }
         format.json { render :show, status: :created, location: @event }
       else
-        format.html { redirect_to new_user_session_path, @event_attending.errors, notice: 'You need to login' }
+        format.html { redirect_to event_path(params[:attended_event_id]), alert: 'Something is wrong. Check if your name already exists in the attendees list' }
         format.json { render json: @event_attending.errors, status: :unprocessable_entity }
       end
     end
