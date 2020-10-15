3d868b18acea9d4066a8ab428c3c3254dd44259d
Abdullah Numan <anewman15@hotmail.com>
2020-10-15 18:12:53 +0600

    Fix UsersController test

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,9 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
-  describe 'GET #show/:id' do
+  describe 'GET #show' do
     it 'renders the user show page' do
-      get :show
+      @user = FactoryBot.create(:user)
+      sign_in(@user)
+      get :show, params: { id: @user.id }
 
       expect(response).to render_template('show')
     end
